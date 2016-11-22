d4f6d10493977e2bb0d38146db93be108f206226
Jonathan Chin <jonathankchin@gmail.com>
2016-11-22 18:27:20 -0500

    finalized functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var wordBank = ["school", "running", "loud", "hand", "skyscraper", "construction
 
 // MESSAGES TO USER
 var winnerMessage = "Congrats, you win!",
-    loserMessage = "Sorry, out of tries. Please try again.",
+    loserMessage = "Sorry, out of tries. Press any key to try again.",
     letterAlreadyPicked = "Sorry, that letter was already used.",
     letterNotExist = "Sorry, that letter does not exist in the word.";
 
@@ -12,7 +12,7 @@ var losesCounter = 0,
     winsCounter = 0,
     guessesCounter = 0,
     wordCounter = 0,
-    incorrectTries = 5; // if wordCounter = word.length === 'YOU WIN'
+    incorrectTries = 5;
 
 var selectedWord = wordBank[Math.floor(Math.random() * arrLength)];
 var splitWord = selectedWord.split("");
@@ -47,6 +47,10 @@ function loseGameNotify() {
 
 // create button to reset page?? http://stackoverflow.com/questions/6666363/is-it-possible-to-clear-a-form-an-reset-reload-the-page-with-one-button
 
+function refreshPage() {
+  window.location.reload(true);
+}
+
 document.onkeyup = function() {
 
         var letter = event.key
@@ -59,15 +63,17 @@ document.onkeyup = function() {
             // need to create a condition to stop program all together and reset page
             if ( incorrectTries <= 0 ) {
               incorrectTries = 0;
+
               document.getElementById('triesMessage').innerHTML = incorrectTries;
-              document.getElementById('usedWrongLetters').innerHTML = usedWrongLetters;
+              document.getElementById('usedLettersWrong').innerHTML = usedWrongLetters;
+
+              refreshPage();
 
             } else if ( incorrectTries  === 1){
                   usedWrongLetters.push(userInput);
                   document.getElementById('usedLettersWrong').innerHTML = usedWrongLetters;
                   incorrectTries -=1;
                   document.getElementById('triesMessage').innerHTML = incorrectTries;
-
                   loseGameNotify();
             } else if ((splitWord.indexOf(userInput) > -1) && (correctLetters.indexOf(userInput) === -1)) {
                 // prevents users from selecting letters already selected
@@ -99,71 +105,3 @@ document.onkeyup = function() {
             }
         }
     }
-    // push selection to an array that stores correct letters
-    // use a loop to check against it
-
-
-// placeholderSplit.splice(splitWord.indexOf(userInput), 1, splitWord[splitWord.indexOf(userInput)])
-// console.log(placeholderSplit);
-// wordCounter += 1;
-// winnerNotification();
-
-// console.log(selectedWord);
-
-//
-// function game() {
-//   var selectedWord = wordBank[Math.floor(Math.random() * arrLength )];
-//   var splitWord = selectedWord.split("");
-//
-// }
-
-// create a "last letter selected" indicator
-// use different color text to indicate last letter guessed
-
-// String.prototype.replaceAt = function (index, character) {
-//     return this.substr(0, index) + character + this.substr(index + character.length);
-// }
-
-
-// if guesses === max fail attempts { alert you lose }
-
-
-// PRESS ANY KEY TO GET STARTED
-
-// var selectedWord = wordBank[Math.floor(Math.random() * arrLength )];
-// prompt user for letters
-// var askUser = prompt("Please enter a letter.")
-
-// while userCounter > selectedWord.length keep running game
-// when userCounter
-
-// https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-// 48 - 90
-// a - z
-// if (event.key > )
-
-// CREATE A WELCOME SCREEN??
-
-// http://www.w3schools.com/charsets/ref_html_ascii.asp
-// 97 - 122
-// a - z
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt
-
-// var word = "replace".split("");
-// console.log(word);
-// var placeholder = "-------".split("");
-// placeholder.splice(2, 1, word[2]);
-// placeholder.splice(6, 1, word[6]);
-// console.log(placeholder)
-
-
-// randomword library = http://www.setgetgo.com/randomword/
-
-// replace with regex: https://www.tutorialspoint.com/javascript/string_replace.htm
-
-// set method: http://stackoverflow.com/questions/7452772/java-arraylist-replace-at-specific-index
-
-// http://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
