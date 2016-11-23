1f5e81ab3d7e7594e1d68060d54ae06b87307afa
Jonathan Chin <jonathankchin@gmail.com>
2016-11-22 19:19:12 -0500

    updated functionality and cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,12 @@ var winnerMessage = "Congrats, you win!",
     letterAlreadyPicked = "Sorry, that letter was already used.",
     letterNotExist = "Sorry, that letter does not exist in the word.";
 
+// DOM
+var targetGameMessage = document.getElementById('gameMessage'),
+    targetWrongLetters = document.getElementById('usedLettersWrong'),
+    targetGameSpace = document.getElementById('userGameSpace'),
+    targetTriesMessage = document.getElementById('triesMessage');
+
 // USER COUNTERS
 var losesCounter = 0,
     winsCounter = 0,
@@ -24,7 +30,7 @@ for (var i = 0; i < selectedWordLength; i++) {
     placeholder += "-";
 }
 var placeholderSplit = placeholder.split('');
-document.getElementById('userGameSpace').innerHTML = placeholder;
+targetGameSpace.innerHTML = placeholder;
 
 var usedWrongLetters = [],
     correctLetters = [];
@@ -32,13 +38,13 @@ var usedWrongLetters = [],
 function winnerNotification() {
     if (wordCounter === selectedWordLength) {
         // console.log(winnerMessage)
-        document.getElementById('gameMessage').innerHTML = winnerMessage;
+        targetGameMessage.innerHTML = winnerMessage;
     }
 }
 
 function loseGameNotify() {
-    document.getElementById('gameMessage').innerHTML = loserMessage;
-  }
+    targetGameMessage.innerHTML = loserMessage;
+}
 
 // Next, we give JavaScript a function to execute when onkeyup event fires.
 
@@ -48,60 +54,60 @@ function loseGameNotify() {
 // create button to reset page?? http://stackoverflow.com/questions/6666363/is-it-possible-to-clear-a-form-an-reset-reload-the-page-with-one-button
 
 function refreshPage() {
-  window.location.reload(true);
+    window.location.reload(true);
 }
 
-document.onkeyup = function() {
+document.onkeyup = function () {
 
-        var letter = event.key
+    var letter = event.key
 
-        // will only print out keypress if a letter is pressed on the keyboard
-        if (letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) {
-            var userInput = event.key
-                // console.log(userInput);
+    // will only print out keypress if a letter is pressed on the keyboard
+    if (letter.charCodeAt(0) > 96 && letter.charCodeAt(0) < 123) {
+        var userInput = event.key
+            // console.log(userInput);
 
-            // need to create a condition to stop program all together and reset page
-            if ( incorrectTries <= 0 ) {
-              incorrectTries = 0;
+        // need to create a condition to stop program all together and reset page
+        if (incorrectTries <= 0) {
+            incorrectTries = 0;
 
-              document.getElementById('triesMessage').innerHTML = incorrectTries;
-              document.getElementById('usedLettersWrong').innerHTML = usedWrongLetters;
+            targetTriesMessage.innerHTML = incorrectTries;
+            targetWrongLetters.innerHTML = usedWrongLetters;
 
-              refreshPage();
+            refreshPage();
 
-            } else if ( incorrectTries  === 1){
-                  usedWrongLetters.push(userInput);
-                  document.getElementById('usedLettersWrong').innerHTML = usedWrongLetters;
-                  incorrectTries -=1;
-                  document.getElementById('triesMessage').innerHTML = incorrectTries;
-                  loseGameNotify();
-            } else if ((splitWord.indexOf(userInput) > -1) && (correctLetters.indexOf(userInput) === -1)) {
-                // prevents users from selecting letters already selected
+        } else if (incorrectTries === 1) {
+            usedWrongLetters.push(userInput);
+            targetWrongLetters.innerHTML = usedWrongLetters;
+            incorrectTries -= 1;
+            targetTriesMessage.innerHTML = incorrectTries;
+            loseGameNotify();
+        } else if ((splitWord.indexOf(userInput) > -1) && (correctLetters.indexOf(userInput) === -1)) {
+            // prevents users from selecting letters already selected
 
-                for (let i = 0; i < splitWord.length; i++) {
+            for (let i = 0; i < splitWord.length; i++) {
 
-                    if (userInput === selectedWord.charAt(i)) {
-                        placeholderSplit.splice(i, 1, userInput.toUpperCase());
-                        // console.log(placeholderSplit.join(""));
-                        document.getElementById('userGameSpace').innerHTML = placeholderSplit.join("");
-                        wordCounter += 1;
-                        correctLetters.push(userInput);
-                        //console.log(correctLetters);
-                        document.getElementById('gameMessage').innerHTML = "Correct Letter!";
-                        winnerNotification();
-                    }
+                if (userInput === selectedWord.charAt(i)) {
+                    placeholderSplit.splice(i, 1, userInput.toUpperCase());
+                    // console.log(placeholderSplit.join(""));
+                    targetTriesMessage.innerHTML = placeholderSplit.join("");
+                    wordCounter += 1;
+                    correctLetters.push(userInput);
+                    //console.log(correctLetters);
+                    targetGameMessage.innerHTML = "Correct Letter!";
+                    winnerNotification();
                 }
-            } else if (((splitWord.indexOf(userInput > -1)) && (correctLetters.indexOf(userInput) > -1)) || (usedWrongLetters.indexOf(userInput) > -1)) {
-                // console.log("Sorry you have already selected that letter");
-                document.getElementById('gameMessage').innerHTML = letterAlreadyPicked;
-            } else {
-                document.getElementById('gameMessage').innerHTML = letterNotExist;
-                incorrectTries -= 1;
-                document.getElementById('triesMessage').innerHTML = incorrectTries;
-                usedWrongLetters.push(userInput);
-                console.log(usedWrongLetters);
-                document.getElementById("usedLettersWrong").innerHTML = usedWrongLetters;
-                //console.log("Incorrect letter selection");
             }
+        } else if (((splitWord.indexOf(userInput > -1)) && (correctLetters.indexOf(userInput) > -1)) || (usedWrongLetters.indexOf(userInput) > -1)) {
+            // console.log("Sorry you have already selected that letter");
+            targetGameMessage.innerHTML = letterAlreadyPicked;
+        } else {
+            targetGameMessage.innerHTML = letterNotExist;
+            incorrectTries -= 1;
+            targetTriesMessage.innerHTML = incorrectTries;
+            usedWrongLetters.push(userInput);
+            console.log(usedWrongLetters);
+            targetWrongLetters.innerHTML = usedWrongLetters;
+            //console.log("Incorrect letter selection");
         }
     }
+}
