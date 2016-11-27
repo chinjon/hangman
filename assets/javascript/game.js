013c79727eba86bd120f3faaa2c353803768dc87
chinjon <Jonathankchin@gmail.com>
2016-11-26 22:13:13 -0500

    changed font size || added mp3s for 2 tracks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-var wordBank = ["fazers", "accordion", "dimes"],
+var wordBank = ["fazers", "accordion", "dimes", "gazzillion", "villain"],
     arrLength = wordBank.length;
 
-// gazillion - gazillion ear
-// villian - vaudville villian
 
 var selectedWord = wordBank[Math.floor(Math.random() * arrLength)];
 var splitWord = selectedWord.split("");
@@ -36,33 +34,51 @@ var losesCounter = 0,
 
 
 var lyricsObject = {
-  "fazers" : {
-    "lyric full" : "Nowadays it's amazing raising young rule number one: Keep your fazers on stun.",
-    "clue": "Weapon of choice on 'Star Trek' and rhymes with 'laser'.",
-    "lyrics before" : "Nowadays it's amazing raising young rule number one: Keep your ",
-    "lyrics word" : "fazers",
-    "lyrics after" : " on stun.",
-    "track info" : "Fazers",
-    "album info" : "Take Me To Your Leader"
-  },
-  "accordion" : {
-    "lyrics" : "And get more cheese than Doritos, Cheetos or Fritos Slip like Freudian your first and last step to playing yourself like accordion.",
-    "clue" : "A piano that you can squish and uses air to make music.",
-    "lyrics before" : "And get more cheese than Doritos, Cheetos or Fritos Slip like Freudian Your first and last step to playing yourself like",
-    "lyrics word" : " accordion" ,
-    "lyrics after" : ".",
-    "track info" : "Accordion",
-    "album info" : "Madvilliany"
-  },
-  "dimes" : {
-    "lyrics" : "I sell rhymes like dimes the one who mostly keep cash but brag about the broker times.",
-    "clue" : "10 cents",
-    "lyrics before" : "I sell rhymes like ",
-    "lyrics word" : "dimes",
-    "lyrics after" : " the one who mostly keep cash but brag about the broker times.",
-    "track info" : "Rhymes Like Dimes",
-    "album info" : "Operation Doomsday"
-  }
+    "fazers": {
+        "lyric full": "Nowadays it's amazing raising young rule number one: Keep your fazers on stun.",
+        "clue": "Weapon of choice on 'Star Trek' and rhymes with 'laser'.",
+        "lyrics before": "Nowadays it's amazing raising young rule number one: Keep your ",
+        "lyrics word": "fazers",
+        "lyrics after": " on stun.",
+        "track info": "Fazers",
+        "album info": "Take Me To Your Leader"
+    },
+    "accordion": {
+        "lyrics": "And get more cheese than Doritos, Cheetos or Fritos Slip like Freudian your first and last step to playing yourself like accordion.",
+        "clue": "A piano that you can squish and uses air to make music.",
+        "lyrics before": "And get more cheese than Doritos, Cheetos or Fritos Slip like Freudian Your first and last step to playing yourself like",
+        "lyrics word": " accordion",
+        "lyrics after": ".",
+        "track info": "Accordion",
+        "album info": "Madvilliany"
+    },
+    "dimes": {
+        "lyrics": "I sell rhymes like dimes the one who mostly keep cash but brag about the broker times.",
+        "clue": "10 cents",
+        "lyrics before": "I sell rhymes like ",
+        "lyrics word": "dimes",
+        "lyrics after": " the one who mostly keep cash but brag about the broker times.",
+        "track info": "Rhymes Like Dimes",
+        "album info": "Operation Doomsday"
+    },
+    "gazzillion": {
+        "lyrics": "Villain man never ran with krills in his hand and won't stop rockin' til he clocked in a gazillion grand tillin' the wasteland sands raps on backs of treasure maps, stacks to the ceilin' fan.",
+        "clue": "More than a billion.",
+        "lyrics before": "Villain man never ran with krills in his hand and won't stop rockin' til he clocked in a",
+        "lyrics word": " gazzillion ",
+        "lyrics after": "grand tillin' the wasteland sands raps on backs of treasure maps, stacks to the ceilin' fan.",
+        "track info": "Gazzillion Ear",
+        "album info": "Born Like This"
+    },
+    "villain": {
+        "lyrics": "V. Vaughn, the travelin' Vaudeville Villain who don't give a flyin' f*** who ain't not feelin' him watch what ya dealin' him: Ace, king, death card strong-arm the wrong man, pardon the left, god.",
+        "clue": "The bad person in movies.",
+        "lyrics before": "V. Vaughn, the travelin' Vaudeville",
+        "lyrics word": " Villain ",
+        "lyrics after": "who don't give a flyin' f*** who ain't not feelin' him watch what ya dealin' him: Ace, king, death card strong-arm the wrong man, pardon the left, god.",
+        "track info": "Vaudeville Villain",
+        "album info": "Vaudeville Villain"
+    }
 }
 
 
@@ -84,8 +100,8 @@ function winnerNotification() {
         // targetGameMessage.innerHTML = lyricsObject[selectedWord]["lyric"];
         targetBeforeLyrics.innerHTML = "'" + lyricsObject[selectedWord]["lyrics before"];
         targetWordLyrics.innerHTML = lyricsObject[selectedWord]["lyrics word"];
-        targetAfterLyrics.innerHTML = lyricsObject[selectedWord]["lyrics after"]+"'";
-        targetTrackInfo.innerHTML = "Lyrics From: " + lyricsObject[selectedWord]["track info"] + " - "  + "<em>" + lyricsObject[selectedWord]["album info"] +"</em>";
+        targetAfterLyrics.innerHTML = lyricsObject[selectedWord]["lyrics after"] + "'";
+        targetTrackInfo.innerHTML = "Lyrics From: " + lyricsObject[selectedWord]["track info"] + " - " + "<em>" + lyricsObject[selectedWord]["album info"] + "</em>";
 
 
         sound.play()
@@ -97,18 +113,20 @@ function loseGameNotify() {
     targetGameMessage.innerHTML = loserMessage;
 }
 
-$('.parallax-window').parallax({imageSrc: 'assets/images/mf-doom-img2-cut.png'});
+$('.parallax-window').parallax({
+    imageSrc: 'assets/images/mf-doom-img2-cut.png'
+});
 
 $(document).ready(function() {
-  $("#topSplash").hide();
-  $("#topSplash").fadeIn(2700, "linear");
+    $("#topSplash").hide();
+    $("#topSplash").fadeIn(2700, "linear");
 
-  $("#wordClue").text(lyricsObject[selectedWord]["clue"]);
+    $("#wordClue").text(lyricsObject[selectedWord]["clue"]);
 
 });
 
 var sound = new Howl({
-  src: ['assets/sound/' + selectedWord + '-cut.mp3']
+    src: ['assets/sound/' + selectedWord + '-cut.mp3']
 });
 
 
@@ -123,7 +141,7 @@ function refreshPage() {
     window.location.reload(true);
 }
 
-document.onkeyup = function () {
+document.onkeyup = function() {
 
     var letter = event.key
 
